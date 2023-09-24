c7f6e913f215ff00af4b501e26849f2ec9ed034a
DESKTOP-IF939KR\ASUS <PLHS>
2023-09-24 18:55:34 +0700

    delete none used file

--- cypress/step_definitions/login_step.js
+++ cypress/step_definitions/login_step.js
@@ -1,24 +1,29 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 require('../support/actions/authen')
-Given('The login page in Subdomain site is opened successfull', () => {
-    cy.visit(Cypress.env("subDomainLink"))
-})
-Given('The login page in Primary Domain site is opened successfull', () => {
-    cy.visit(Cypress.env("primaryDomainLink"))
+
+Given('The login page in {string} site is opened successfull', (env) => {
+    switch ((env + '').trim().toLocaleLowerCase()) {
+        case 'primary-domain':
+            cy.visit(Cypress.env("primaryDomainLink"))
+            break;
+        case 'sub-domain':
+            cy.visit(Cypress.env("subDomainLink"))
+            break;
+    }
 })
 
 When('Login with email: {string} and password: {string}', (email, password) => {
-        cy.loginSubDomain(email, password)
+    cy.loginSubDomain(email, password)
 })
 
 When('Login with email: {string}, domain: {string} and password: {string}', (email, domain, password) => {
-        cy.loginPrimaryDomain(email, domain, password)
+    cy.loginPrimaryDomain(email, domain, password)
 })
-
 
 Then('Verify url home page', () => {
-        cy.url().should('include', 'new_session=true')
+    cy.url().should('include', 'new_session=true')
 })
-Then('the message {string} should be displayed', (message) =>{
+
+Then('the message {string} should be displayed', (message) => {
     cy.contains(message)
 })
